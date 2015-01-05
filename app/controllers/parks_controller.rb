e59469b109dcad4389f2a669133dbc51d7a76f89
Connor Stauffer <connor.paul.stauffer@gmail.com>
2015-01-05 23:25:51 +0000

    Add user show and index

--- app/controllers/parks_controller.rb
+++ app/controllers/parks_controller.rb
@@ -1,2 +1,11 @@
 class ParksController < ApplicationController
+  
+  def index
+    @parks = Park.paginate(page: params[:page])
+  end
+  
+  def show
+    @park = Park.find(params[:id])
+  end
+  
 end
